5a6157e0e3855f8b75a4c3ac7a4d522147b27eea
reactivaperu <jorge.muvez@gmail.com>
2021-04-15 08:52:27 -0500

    intento mñn

--- config/app.php
+++ config/app.php
@@ -229,7 +229,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Socialte' => Laravel\Socialite\Facades\Socialite::class,
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
 
     ],
 
